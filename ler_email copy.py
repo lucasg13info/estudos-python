dc0910df63ec7664d73b56730640fb8e1ce1504c
Lucas Estefano <lucccasestefano_@hotmail.com>
2021-06-20 23:38:09 -0300

    Ajustes na busca do e-mail e identação

--- ler_email copy.py
+++ ler_email copy.py
@@ -1,31 +1,33 @@
+#Libs para tratativa com e-mail
 import smtplib
 import time
 import imaplib
 import email
 import traceback 
-# -------------------------------------------------
-#
-# Utility to read email from Gmail Using Python
-#
-# ------------------------------------------------
+
+#Credenciais, porta e SMTP padrão gmail
 ORG_EMAIL = "lucccasestefano1@gmail.com" 
 FROM_EMAIL = "lucccasestefano1@gmail.com"
 FROM_PWD = "tirodefuzil762" 
 SMTP_SERVER = "imap.gmail.com" 
 SMTP_PORT = 993
 
+#função que faz a leitura
 def read_email_from_gmail():
     try:
         mail = imaplib.IMAP4_SSL(SMTP_SERVER)
         mail.login(FROM_EMAIL,FROM_PWD)
         mail.select('inbox')
+        #Feito a conexão e selecionando a caixa de e-mail "inbox" acima
 
+        #lê todos e-mails e adiciona um Id a cada um deles 
         data = mail.search(None, 'ALL')
         mail_ids = data[1]
         id_list = mail_ids[0].split()   
         first_email_id = int(id_list[0])
         latest_email_id = int(id_list[-1])
 
+        #Busca um e-mail com Id específico através do rptocolo RFC822
         for i in range(latest_email_id,first_email_id, -1):
             data = mail.fetch(str(i), '(RFC822)' )
             for response_part in data:
