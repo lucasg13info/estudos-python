6b9f30ca8df6806f4f2e0e9f1a150dee5628da46
Lucas Estefano <lucccasestefano_@hotmail.com>
2021-07-01 11:40:05 -0300

    Fim mundo 2 - while, for, if elif

--- mundo2/aula015.py
+++ mundo2/aula015.py
@@ -10,7 +10,18 @@
 # print('Acabou')
 
 #2 
-n = cont = 0
-while cont < 3 : 
-    n = int(input('Leia um número: '))
-    cont += 1
+# n = cont = 0
+# while cont < 3 : 
+#     n = int(input('Leia um número: '))
+#     cont += 1
+
+#3 Break
+
+n = s= 0
+while True:
+    n = int(input( 'Digite um número: '))
+    if n == 999:
+        break
+    s += n
+print('A soma vale {}'.format(s))
+print(f'A soma vale {s}')
