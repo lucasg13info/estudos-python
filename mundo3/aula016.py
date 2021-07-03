e81b2950a8d6f125797c7dca84e5b0e9e06ff0a6
Lucas Estefano <lucccasestefano_@hotmail.com>
2021-07-03 13:20:49 -0300

    tuplas

---
+++ mundo3/aula016.py
@@ -0,0 +1,27 @@
+#TUPLAS usado entre parenteses  (   )
+
+#lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim', 'Batata Frita')
+
+#1
+# for comida in lanche:
+    # print(f'Eu vou comer {comida}')
+# print('Comi pacas')
+
+
+#2 Colocando em ordem alfabética
+#print(sorted(lanche))
+
+
+#3 Concatenando as túplas
+#a = (1,3,6)
+#b = (4,5,7)
+#c= a +b
+#print(len(c))  #Mostra a quantidade de caracteres
+#print(c.count(5)) #Verifica quantas vezes aparece o número 5 
+#print(c.index(7)) #Mostra a posição do número 7
+
+
+#3 Apagando uma variável
+c = ('Nome', 'idade', 'sexo')
+del c
+print(c)
